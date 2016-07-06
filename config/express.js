eb02e521935cc58f1415165a8132801bbcd9d6cd
Maycon <mayconds000@gmail.com>
2016-07-06 17:40:08 -0300

    Adicionado body-parser e method-override ./config/express.js para que funcione os metos PUT e DELETE

--- config/express.js
+++ config/express.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var home = require('../app/routes/home');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+
 
 module.exports = function() {
   var app = express();
@@ -14,6 +16,10 @@ module.exports = function() {
   //tem que ser em baixo do middleware express.static
   app.set('view engine', 'ejs');
   app.set('views', './app/views');
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
 
   load('models', {cwd: 'app'})
     .then('controllers')
